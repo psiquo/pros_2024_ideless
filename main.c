f91f46fa31bb8b31881f455e0824a84471023aa8
Davide Rusconi <daviderusconi1@gmail.com>
2024-04-11 16:44:43 +0200

    CMSIS interrupts

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 
 
 uint32_t ticks;
+uint32_t counter = 0;
 
 uint32_t leds[] = {12,13,14,15};
 
@@ -25,8 +26,17 @@ void delay(uint32_t ms) {
 
 void interrupt_setup(void){
     RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
+    SYSCFG->EXTICR[0] &= ~SYSCFG_EXTICR1_EXTI0_Msk;
+    SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI0_PA; 
 
+    EXTI->IMR |= EXTI_IMR_IM0;
+    EXTI->RTSR &= (~EXTI_RTSR_TR0_Msk);
+    EXTI->FTSR |= EXTI_FTSR_TR0_Msk;
+
+    NVIC_SetPriority(EXTI0_IRQn, 0);
+    NVIC_EnableIRQ(EXTI0_IRQn);
 }
+
 void gpio_setup(void) {
     RCC->AHB1ENR |= (1 << RCC_AHB1ENR_GPIODEN_Pos);
     
@@ -38,6 +48,18 @@ void gpio_setup(void) {
     GPIOD->MODER |= (1 << GPIO_MODER_MODER13_Pos);
     GPIOD->MODER |= (1 << GPIO_MODER_MODER14_Pos);
     GPIOD->MODER |= (1 << GPIO_MODER_MODER15_Pos);
+
+    GPIOA->MODER &= (~GPIO_MODER_MODE10_Msk);
+    GPIOA->PUPDR &= (~GPIO_PUPDR_PUPD0_Msk);
+    GPIOA->PUPDR |= (0x10U << GPIO_PUPDR_PUPD0_Pos);
+}
+
+void EXTI0_IRQHandler(void) {
+  if (EXTI->PR & 1) {
+    EXTI->PR |= 1;
+    GPIOD->ODR ^= (1 << leds[counter % 4]); 
+    counter++;
+  }
 }
 
 void main(void) {
@@ -45,13 +67,14 @@ void main(void) {
     __enable_irq();
 
     gpio_setup();
+    interrupt_setup();
 
-    uint32_t counter = 0;
+    
 
-    while (1)
-    {   
-        GPIOD->ODR ^= (1 << leds[counter++ % 4]);
-        delay(100);
-    }
+     while (1)
+     {   
+    //     GPIOD->ODR ^= (1 << leds[counter++ % 4]);
+    //     delay(100);
+     }
     
 }
